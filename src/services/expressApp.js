9c3875420396a0b7c480437717094d19e19d0aeb
sayoadesokan <sayosokan@gmail.com>
2023-06-11 14:30:31 +0100

    cors access

--- src/services/expressApp.js
+++ src/services/expressApp.js
@@ -8,7 +8,11 @@ const { router } = require('../routes/productRoutes');
 module.exports = (app) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  app.use(cors());
+  app.use(
+    cors({
+      origin: ['http://localhost:5000', 'http://localhost:3000'],
+    })
+  );
   app.use(helmet());
 
   //admin routes
