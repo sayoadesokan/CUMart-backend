31d0d21ee8bea80987098296e3afd4034c4233a9
sayoadesokan <sayosokan@gmail.com>
2023-06-12 14:24:29 +0100

    cors all

--- src/services/expressApp.js
+++ src/services/expressApp.js
@@ -10,7 +10,7 @@ module.exports = (app) => {
   app.use(express.urlencoded({ extended: true }));
   app.use(
     cors({
-      origin: ['http://localhost:5173'],
+      origin: '*',
     })
   );
   app.use(helmet());
